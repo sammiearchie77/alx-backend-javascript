9361232740fb70074fda7815bd136370135d3285
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-06-29 15:23:59 +0100

    README.md and 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-underscore-dangle */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
