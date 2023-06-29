086a55cce8d1256f9050ac8519330c44fa41e44d
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-06-29 15:11:43 +0100

    README.md and 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,15 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  set maxStudentsSize(newMaxStudentsSize) {
+    if (typeof newMaxStudentsSize === 'number') {
+        this._maxStudentsSize = newMaxStudentsSize;
+    }
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+}
