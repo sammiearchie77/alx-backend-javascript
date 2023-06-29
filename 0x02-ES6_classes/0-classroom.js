9ad0f0ec9d478ac3796357cf58e8e83a27794f93
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-06-29 17:15:08 +0100

    Classes Task

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-underscore-dangle */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
