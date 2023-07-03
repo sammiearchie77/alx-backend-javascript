56430f95beb0450600c3fe6dc14f94ae77162405
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-07-03 19:44:23 +0100

    Trailing spaces not allowed fixed

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 /**
  * A weak map of endpoints and the number of calls made.
  */
@@ -12,9 +13,9 @@ const MAX_ENDPOINT_CALLS = 5;
  * 	protocol: String,
  * 	name; String,
  * }} endpoint - The endpoint to make a request to.
- */ 
+ */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) throw new Error('Endpoint load is high');
 }
