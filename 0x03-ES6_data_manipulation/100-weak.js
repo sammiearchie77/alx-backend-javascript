e280e10cb1cadb86d24305a8d1fd98aeda1dc4a6
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-07-03 12:35:00 +0100

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,20 @@
+/**
+ * A weak map of endpoints and the number of calls made.
+ */
+export const weakmap = new WeakMap();
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
+/**
+ * Tracks the number of alls made to am API's endpoint.
+ * @param {{
+ * 	protocol: String,
+ * 	name; String,
+ * }} endpoint - The endpoint to make a request to.
+ */ 
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) throw new Error('Endpoint load is high');
+}
