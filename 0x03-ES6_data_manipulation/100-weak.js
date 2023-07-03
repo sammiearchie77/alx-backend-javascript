cd126193a39b2cc15c5e44deb03a4979fe6f6cb5
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-07-03 20:25:07 +0100

    Refactor 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,19 @@
-/* eslint-disable no-tabs */
-/**
- * A weak map of endpoints and the number of calls made.
- */
-export const weakmap = new WeakMap();
-/**
- * The maximum number of calls for an endpoint.
- */
-const MAX_ENDPOINT_CALLS = 5;
-/**
- * Tracks the number of alls made to am API's endpoint.
- * @param {{
- * 	protocol: String,
- * 	name; String,
- * }} endpoint - The endpoint to make a request to.
- */
-export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) throw new Error('Endpoint load is high');
+// WeakMap instance
+const weakMap = new WeakMap();
+
+// Function to track the number of times queryAPI is called for each endpoints
+const queryAPI = endpoint => {
+	// Get the count for the endpoint from the weakMap
+	let count = weakMap.get(endpoint) || 0;
+
+	// Increment the count
+	count++;
+
+	// Update the count in the weakMap
+	weakMap.set(endpoint, count);
+
+	// Check if the count is greater than or equal to 5
+	if (count >= 5) throw new Error('Endpoint load too high');
 }
+
+export { weakMap, queryAPI };
